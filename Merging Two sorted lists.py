44a11f7d7630f09f1ee9b7b23811d3850f9cb1ec
Chandan Prajwal <76419264+ChandanSouL@users.noreply.github.com>
2024-08-15 14:51:26 +0530

    Merging Two sorted List nodes

---
+++ Merging Two sorted lists.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list2.next, list1)
+            return list2
+
+#first checking if the list is empty or not if it is empty returing other list
+#next comparing the value of list1 with list2 if it is less then
+#using recursion assigning the list1.next to list2
