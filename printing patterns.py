3ca66ae36389c21284822345cec19f5b4f450380
Chandan Prajwal <76419264+ChandanSouL@users.noreply.github.com>
2024-08-26 13:11:08 +0530

    printing patterns

--- printing patterns.py
+++ printing patterns.py
@@ -10,6 +10,13 @@ for i in range(1,n+1):
 
 #instead of * print numbers
 #use nums =1 and increment nums at the end of ith loop
+s = 65
+n = 5
+for i in range(1,n):
+    for j in range(1,i+1):
+        print(chr(s),end = ' ')
+        s+=1
+    print('')
 
 
 
@@ -18,4 +25,32 @@ n = 5
 for i in range(n,0,-1):
     for j in range(1,i+1):
         print('* ',end = ' ')
+    print('')
+
+
+#full pyramid
+
+
+n = 7
+for i in range(1,n+1):      #(n,0,-1)
+    for j in range(n-i):
+        print(" ",end = '')
+    
+    for k in range(1,2*i):  #(2*i-1)
+        print("*",end = ' ')
+    print(' ')
+
+
+
+#printing alphabets
+
+alpha = 65
+n = 4
+for i in range(0,n):
+    print(" "*(n-i),end = ' ')
+    
+    for j in range(0,i+1):
+        print(chr(alpha),end = ' ')
+        alpha+=1 
+    alpha = 65
     print('')
